cde58d26484abda6cf6ce1dc336c079aacde988f
김동삼 <creaddiscans@gmail.com>
2019-05-23 17:25:54 +0900

    templates 위치 수정

--- servicestarter/settings.py
+++ servicestarter/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'servicestarter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'react/public')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
