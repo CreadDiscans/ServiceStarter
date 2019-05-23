d187ad94c704a8287c6a89bfb260fccb5c9691e4
Creaddiscans <Creaddiscans@gmail.com>
2019-05-23 23:26:49 +0900

    install skip option 추가

--- api/management/commands/build.py
+++ api/management/commands/build.py
@@ -1,14 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
+import time
 
 class Command(BaseCommand):
 
   def add_arguments(self, parser):
-    pass
+    parser.add_argument('--skip-install',
+      action="store_true",
+      dest='skip_install',
+      default=False,
+      help='Skip npm, pip install')
 
   def handle(self, *args, **options):
-    os.system('pip install -U -r requirements.txt')
-    os.system('cd react && npm install')
+    start = time.time()
+    if not options['skip_install']:
+      os.system('cd react && npm install')
+      os.system('pip install -U -r requirements.txt')
     os.system('cd react && npm run build')
+    os.system('cd react && npm run build:server')
     os.system('python manage.py migrate --settings=servicestarter.production_settings')
     os.system('python manage.py collectstatic --no-input --settings=servicestarter.production_settings')
+    print('build success, time : ', time.time() - start)
