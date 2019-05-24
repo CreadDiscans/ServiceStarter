a983b7e3372a00c80f823fb1abe2b95945fa395f
김동삼 <creaddiscans@gmail.com>
2019-05-24 11:25:29 +0900

    staruml-django 업데이트 반영

--- servicestarter/urls.py
+++ servicestarter/urls.py
@@ -1,33 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-from .views import *
+from .views import index, UserViewSet, GroupViewSet
   
 router = routers.DefaultRouter()
-router.register(r'home/user', UserViewSet)
-router.register(r'home/group', GroupViewSet)
+router.register(r'api-user', UserViewSet)
+router.register(r'api-group', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token-auth/', obtain_jwt_token),
     path('api/token-refresh/', refresh_jwt_token),
     path('api/token-verify/', verify_jwt_token),
-    path('', index),
-    path('', include('api.urls'))
+    path('api-', include('api.urls')),
+    path('', index)
 ]
 urlpatterns += router.urls
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += [
         path('swagger/', get_swagger_view(title='Pastebin API')),
         path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     ]
 
-urlpatterns +=[url('^', index)]
+urlpatterns +=[url('', index)]
