3a48b981d32db4046f2293dad617eb117c1beb08
김동삼 <creaddiscans@gmail.com>
2019-07-02 15:22:34 +0900

    withdone add withRouter

--- react/src/app/core/connection.ts
+++ react/src/app/core/connection.ts
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router";
 
 export const connectWithDone:any = (mapStateToProps:any, mapDispatchToProps:any, compnent:any) => {
-  const conn:any = connect(mapStateToProps, mapDispatchToProps)(compnent)
+  const conn:any = connect(mapStateToProps, mapDispatchToProps)(withRouter(compnent))
   return withDone(conn);
 }
 
