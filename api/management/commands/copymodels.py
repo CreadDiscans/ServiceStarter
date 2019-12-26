0b251e47d4b48a2fffd3f72df12889fe33dcea23
dongsam.kim <dongsam.kim@juice.co.kr>
2019-12-26 13:07:18 +0900

    typescript api type generate

--- api/management/commands/copymodels.py
+++ api/management/commands/copymodels.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 import os
 
 class Command(BaseCommand):
@@ -11,13 +12,19 @@ class Command(BaseCommand):
 
   def copyModels(self):
     body = 'from django.db import models\n\n'
+    type_body = "import * as custom from './custom.types'\n\n"
       
     for root, _, files in os.walk('models'):
       for file in files:
         fullpath = os.path.join(root, file)
         with open(fullpath, 'r', encoding='utf-8') as f:
           content = f.read()
-        content = '\n'.join(content.split('\n')[4:])
-        body += content
+        if '.py' in file:
+          content = '\n'.join(content.split('\n')[4:])
+          body += content
+        elif '.d.ts' in file:
+          type_body += content + '\n'
+    with open(os.path.join(settings.REACT_API_TYPE_PATH, 'api.types.d.ts'), 'w', encoding='utf-8') as f:
+      f.write(type_body)
     with open('api/models.py', 'w', encoding='utf-8') as f:
       f.write(body)
