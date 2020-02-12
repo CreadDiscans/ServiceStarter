d35bf10c84c9d6a50576787d3965eb648fb365c8
dongsam.kim <dongsam.kim@juice.co.kr>
2020-02-13 00:16:55 +0900

    sw update

--- react/public/assets/sw.js
+++ react/public/assets/sw.js
@@ -2,7 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.8.1/firebase-messaging.js'); 
 
 firebase.initializeApp({ 
-    'messagingSenderId': '460789091763' 
+    apiKey: "AIzaSyCmfVBPAbeU76f-M1jpkMbOvuqJ1eF-dBE",
+    authDomain: "servicestarter-770d0.firebaseapp.com",
+    databaseURL: "https://servicestarter-770d0.firebaseio.com",
+    projectId: "servicestarter-770d0",
+    storageBucket: "servicestarter-770d0.appspot.com",
+    messagingSenderId: "460789091763",
+    appId: "1:460789091763:web:358e2a97967b45caff0fc6",
+    measurementId: "G-4QT1LCRVJ0"
 }); 
 const messaging = firebase.messaging(); 
 
