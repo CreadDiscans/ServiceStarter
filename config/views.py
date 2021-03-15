5f5a4c37a9d5be8aae1c5742085f12a57a45fb1e
macos <macos@macosui-iMac.local>
2021-03-15 11:32:02 +0900

    email 없는 경우 테스트 코드 주석처리

--- config/views.py
+++ config/views.py
@@ -120,8 +120,7 @@ class SocialSiginViewSet(viewsets.ViewSet):
         uid = request.data['uid']
         token = request.data['token']
         name = request.data['name']
-        email = request.data['email'] if 'email' in request.data else sns + '@' + uid
-        username = email
+        username = request.data['email'] if 'email' in request.data else sns + '@' + uid
         if sns == 'google':
             r = requests.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json',headers={
                 'Authorization': 'Bearer '+token})
