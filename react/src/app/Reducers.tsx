a1f47421ab652c5e244e693c5cdf081be5c4278a
eric.ha <eric.ha@juice.co.kr>
2021-03-18 18:51:38 +0900

    Dynamic reudcer(react) 적용 완료

--- react/src/app/Reducers.tsx
+++ react/src/app/Reducers.tsx
@@ -1,19 +1,18 @@
 import { combineReducers } from 'redux';
 import { penderReducer } from 'redux-pender';
-
-import auth, { AuthState } from 'auth/Auth.action';
+import { AuthState } from 'auth/Auth.action';
 import board, { BoardState } from 'board/Board.action';
 import dashboard, { DashboardState } from 'dashboard/Dashboard.action';
 import mypage, { MypageState } from 'mypage/Mypage.action';
 import shared, { SharedState } from 'component/Shared.action';
-export default combineReducers({
-  auth,
+
+export default (asyncReducers={}) => combineReducers({
   board,
   dashboard,
   mypage,
   shared,
   pender: penderReducer,
-  // ...asyncReducers
+  ...asyncReducers
 })
 
 export type RootState = {
