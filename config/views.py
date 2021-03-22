4b897176f573364a32a1a7aa647f73b8342ccebe
dongsam.kim <creaddiscans@gmail.com>
2021-03-22 18:12:16 +0900

    django 3 적용용

--- config/views.py
+++ config/views.py
@@ -10,7 +10,7 @@ from django.template.loader import render_to_string
 from django.middleware.csrf import get_token
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_text
-from django.utils import six
+import six
 from rest_framework import viewsets, status
 from rest_framework import serializers
 from rest_framework.response import Response
