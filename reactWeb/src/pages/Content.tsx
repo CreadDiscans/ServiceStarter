0ae1ce608a137ae7d1b57bb27e63ecd229857aa0
Jong Won Park <comics77@gmail.com>
2021-04-09 13:01:42 +0900

    nextjs

--- reactWeb/src/pages/Content.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Content = () => (
-  <span>Hello! I'm content of this page!</span>
-);
-
-export default Content;
--- reactWeb/src/pages/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFount = () => (
-  <span>Not Found</span>
-);
-
-export default NotFount;
